5fe058df9a39ca5e1d765bc654bcf7de534231c4
Ngoc Anh Nhi Bui <148082202+nhibui32@users.noreply.github.com>
2024-10-19 23:18:30 -0500

    signin/signup/forgotWord pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   const navigate = useNavigate();
   // we can use location.pathname to get their path name and location 
   // console.log(location.pathname)
-  function pathMathRoute(route){
+  function pathMatchRoute(route){
     if(route === location.pathname){
       return true;
     }
@@ -13,15 +13,15 @@ export default function Header() {
   return (
     <div className='bg-white border-b shadow-sm sticky top-0 z-50'>
       {/* position: sticky it means stay as some where, and the say where it will stay. z-50 ( z index of everything, if z-0 on the top, z--1 go under, z-50 just to make sure it always be on top) */}
-      <header className='flex justify-between items-center px-3 max-w-6x1 mx-autov'>
+      <header className='flex justify-between items-center px-3 max-w-6x1 mx-auto'>
         <div>
             <img src="https://static.rdc.moveaws.com/rdc-ui/logos/logo-brand.svg" alt="logo" className='h-5 cursor-pointer' onClick={() => navigate("/")}/>
         </div>
         <div>
             <ul className='flex space-x-10'>
-                <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/") && "text-black border-b-red-500"}`} onClick={() => navigate("/")}>Home</li>
-                <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/offers") && "text-black border-b-red-500"}`} onClick={() => navigate("/offers")}>Offers</li>
-                <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/sign-in") && "text-black border-b-red-500"}`} onClick={() => navigate("/sign-in")}>Sign in</li>
+                <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/") && "text-black border-b-red-500"}`} onClick={() => navigate("/")}>Home</li>
+                <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/offers") && "text-blue-500 border-b-red-600"}`} onClick={() => navigate("/offers")}>Offers</li>
+                <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/sign-in") && " text-black border-b-red-500"}`} onClick={() => navigate("/sign-in")}>Sign in</li>
             </ul>
         </div>
       </header>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaEye,FaEyeSlash } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+import OAuth from '../components/OAuth';
 
 export default function SignUp() {
+  const [showPassword, setShowPassword] = useState(false)
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  })
+  const {name, email, password} = formData;
+  const onChange = (e) => {
+    console.log(e.target.value)
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id] : e.target.value
+    }))
+  }
   return (
-    <div>
-      This is SignUp page 
-    </div>
+    <section>
+      <h1 className='text-3xl text-center mt-6 font-bold'>Sign Up</h1>
+      <div className='flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto'>
+        <div className='md:w-[67%] lg:w-[50%] mb-12 md:mb-6'>
+          <img src="https://plus.unsplash.com/premium_photo-1663089688180-444ff0066e5d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+          alt="key" 
+          className='w-full rounded-2xl'
+          />
+        </div>
+        <div  className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
+          <form>
+            <input type="text" id="name" value={name} onChange={onChange} placeholder="Full Name"
+              className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out'/>
+            <input type="email" id="email" value={email} onChange={onChange} placeholder='Email address'
+            className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out'/>
+            <div className='relative '>
+              <input type={showPassword ? "text" : "password"} id="password" value={password} onChange={onChange} placeholder='Password'
+              className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out'/>
+              {showPassword ? (
+                <FaEye className='absolute right-3 top-3 text-xl cursor-pointer' onClick={()=>{ setShowPassword((prevState) => !prevState)}} 
+                />) : 
+                (<FaEyeSlash className='absolute right-3 top-3 text-xl cursor-pointer' onClick={()=>{ setShowPassword((prevState) => !prevState)}} />)
+              }
+            </div>
+            <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg'>
+            {/* whitespace-nowrap to prevent them to go to the second line! */}
+              <p className='mb-6'>Have an account
+                <Link to="/sign-in" className='text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1'> Sign up</Link>
+              </p>
+              <p>
+              <Link to="/forgot-password" className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out ml-1'>Forgot password?</Link>
+              </p>
+            </div>
+              <button type='submit' className='w-full bg-blue-600 text-white text-sm font-medium px-7 py-3 uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800'>Sign up</button>
+          <div className='flex items-center my-4 before:border-t before:flex-1 before:border-gray-300 after:border-t after:flex-1 after:border-gray-300'>
+            <p className='text-center font-semibold mx-4'>OR</p>
+          </div>
+          <OAuth/>
+          </form>
+        </div>
+      </div>
+    </section>
   )
 }
