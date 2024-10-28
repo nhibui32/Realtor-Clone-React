0e2d6bda6ca401976414eb28dc0fc2acc509054d
Ngoc Anh Nhi Bui <148082202+nhibui32@users.noreply.github.com>
2024-10-28 13:07:13 -0500

    Create Listing Page UI

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
 import PrivateRoute from './components/privateRoute'
+import CreateListing from './pages/CreateListing'
 import Logo from '/favicon.ico'
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer, Bounce } from 'react-toastify';
@@ -26,6 +27,7 @@ function App() {
           </Route>
           <Route path='forgot-password' element={<ForgotPassword/>}/>
           <Route path='offers' element={<Offers/>}/>
+          <Route path='create-listing' element={<CreateListing/>}/>
           <Route path='sign-in' element={<SignIn/>}/>
           <Route path='sign-up' element={<SignUp/>}/>
         </Routes>
